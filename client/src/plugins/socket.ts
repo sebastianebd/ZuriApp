60b8f733198e9c504bb8ee58a2c48bccbb62bf47
Sebastian <sebastian.ebd@gmail.com>
2025-12-22 02:54:49 -0300

    fix: se integra nuevamente transporte en cfg socket.io

--- client/src/plugins/socket.ts
+++ client/src/plugins/socket.ts
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client'
 
 const socketUrl = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3500'
 const socket = io(socketUrl, {
+  transports: ['websocket'],
   withCredentials: true
 })
 
