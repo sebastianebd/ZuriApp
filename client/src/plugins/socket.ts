cd31eb34dc55785a1cd7c2316d55b8aa3e6b26dc
Sebastian <sebastian.ebd@gmail.com>
2025-12-22 03:01:36 -0300

    fix: se aplica cfg en nginx

--- client/src/plugins/socket.ts
+++ client/src/plugins/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
-const socketUrl = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3500'
+const socketUrl =
+  import.meta.env.VITE_SOCKET_URL || (import.meta.env.DEV ? 'http://localhost:3500' : undefined)
 const socket = io(socketUrl, {
   transports: ['websocket'],
   withCredentials: true
