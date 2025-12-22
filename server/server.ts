24d573836c11d70680f1c9b31c92c42661dd4336
Sebastian <sebastian.ebd@gmail.com>
2025-12-22 03:07:18 -0300

    fix: minor fix socket.io

--- server/server.ts
+++ server/server.ts
@@ -13,8 +13,13 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer(app);
 
 const allowedOrigins = process.env.CLIENT_URL
-  ? process.env.CLIENT_URL.split(",")
+  ? process.env.CLIENT_URL.split(",").map((url) =>
+      url.trim().replace(/\/$/, "")
+    )
   : ["http://localhost:5173", "http://localhost:4173", "http://localhost:5174"];
+
+console.log("Allowed Origins for Socket.io:", allowedOrigins);
+
 socketIO.init(server, allowedOrigins);
 
 connectDB();
