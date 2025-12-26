a81db4023a02c3030b9f60aec5aa650da7e57159
Sebastian <sebastian.ebd@gmail.com>
2025-12-26 09:06:05 -0300

    update: se configura 1 browsear para testear en lugar de 3

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -27,18 +27,25 @@ export default defineConfig({
           stderr: 'pipe'
         }
       }),
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    }
-  ]
+  projects: process.env.CI
+    ? [
+        {
+          name: 'chromium',
+          use: { ...devices['Desktop Chrome'] }
+        }
+      ]
+    : [
+        {
+          name: 'chromium',
+          use: { ...devices['Desktop Chrome'] }
+        },
+        {
+          name: 'firefox',
+          use: { ...devices['Desktop Firefox'] }
+        },
+        {
+          name: 'webkit',
+          use: { ...devices['Desktop Safari'] }
+        }
+      ]
 })
