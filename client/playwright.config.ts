c0ba388a173305adf0ab6e489838bbd9451634fe
Sebastian <sebastian.ebd@gmail.com>
2025-12-26 13:09:39 -0300

    fix: se integra storagestate a cada proyecto en playwright

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -32,21 +32,33 @@ export default defineConfig({
     ? [
         {
           name: 'chromium',
-          use: { ...devices['Desktop Chrome'] }
+          use: {
+            ...devices['Desktop Chrome'],
+            storageState: 'playwright/.auth/user.json'
+          }
         }
       ]
     : [
         {
           name: 'chromium',
-          use: { ...devices['Desktop Chrome'] }
+          use: {
+            ...devices['Desktop Chrome'],
+            storageState: 'playwright/.auth/user.json'
+          }
         },
         {
           name: 'firefox',
-          use: { ...devices['Desktop Firefox'] }
+          use: {
+            ...devices['Desktop Firefox'],
+            storageState: 'playwright/.auth/user.json'
+          }
         },
         {
           name: 'webkit',
-          use: { ...devices['Desktop Safari'] }
+          use: {
+            ...devices['Desktop Safari'],
+            storageState: 'playwright/.auth/user.json'
+          }
         }
       ]
 })
